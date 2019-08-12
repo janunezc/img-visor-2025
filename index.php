42b175db81f218c7bd5f70e3cd4995d7350b18ca
Jose Nunez <jose.a.nunez@gmail.com>
2019-08-11 23:06:27 -0600

    LnF Improvements

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$version = "0.1";
+$version = "0.2";
 
 $apiCommand = filter_input(INPUT_GET, "apiCommand");
 if ($apiCommand == "getnames") {
@@ -35,17 +35,24 @@ if ($apiCommand == "getnames") {
     <head>
         <title>img-visor rev. <?php echo $version; ?></title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     </head>
     <body style="text-align:center">
         <h1><pre>img-visor <?php echo $version; ?></pre></h1>
-        <button id="btnREW"> << </button> <button id="btnPause"> || </button> <button id="btnFF"> >> </button>
-        <p id="theContainer">Not Loaded</p>
-        <img id="theImage" style="min-width: 400px; max-width:800px; width:100%; margin:0 auto; display:block" src="screenshot 08-08-2019-204928.png" alt="IMAGE LOADING..." />
-        <script type="text/javascript">
+        <button id="btnREW" class="btn btn-primary"><i class="fas fa-backward"></i></button>
+        <button id="btnPause" class="btn btn-primary"><i class="fas fa-play"></i></button>
+        <button id="btnFF" class="btn btn-primary"><i class="fas fa-forward"></i></button>
+        <pre id="imgData"> - </pre>
+        <img id="theImage" style="min-width: 400px; max-width:800px; width:100%; margin:0 auto; display:block" src="" alt="..." />
 
+        <script src="https://kit.fontawesome.com/db2d8f91f5.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+        <script type="text/javascript">
             $(document).ready(function () {
                 var filesArray = [];
                 var timer;
@@ -60,15 +67,15 @@ if ($apiCommand == "getnames") {
                         .always(function () {});
                 var index = 0;
                 var direction = "FF";
-                var paused = false;
+                var paused = true;
 
                 $("#theImage").on("load", setTimer);
                 $("#btnPause").click(function () {
                     paused = !paused;
                     if (paused) {
-                        $("#btnPause").text(" > ");
+                        $("#btnPause i").removeClass("fa-pause").addClass("fa-play");
                     } else {
-                        $("#btnPause").text(" || ");
+                        $("#btnPause i").removeClass("fa-play").addClass("fa-pause");
                     }
                     setTimer();
                 });
@@ -100,15 +107,13 @@ if ($apiCommand == "getnames") {
                 }
 
                 function timerFunction() {
-                    $("#theContainer").html("IDX:" + (index + 1)     + " OF " + filesArray.length + " FILE:" + filesArray[index]);
+                    $("#imgData").html("IDX:" + (index + 1) + " OF " + filesArray.length + "<br />" + filesArray[index]);
 
                     if (!paused) {
                         advance();
                     }
                 }
             });
-
-
         </script>
     </body>
 </html>
