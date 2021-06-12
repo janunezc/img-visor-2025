97e3ec3a38815583251c304b93537206d4c1c2f5
Jose Nunez <jose.a.nunez@gmail.com>
2021-06-11 20:11:07 -0600

    winfrm srvr

---
+++ imgvisor-winfrm/imgvisor-winfrm/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace imgvisor_winfrm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataSet1 = new System.Data.DataSet();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WELCOME TO IMGVISOR AGENT. YOUR SYSTEM IS SECURED.";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 83);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(356, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 105);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.Text = "IMGVISOR-WINFRM";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ imgvisor-winfrm/imgvisor-winfrm/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Configuration;
+
+namespace imgvisor_winfrm
+{
+    public partial class Form1 : Form
+    {
+        //System.Timers.Timer timer = new System.Timers.Timer();
+        System.Windows.Forms.Timer wftimer;
+        const int TIME_TO_MINIMIZE = 15000;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int intervalms = Int32.Parse(ConfigurationManager.AppSettings.Get("intervalms"));
+
+            WriteToFile("Service V.1954 is started at " + DateTime.Now);
+
+            wftimer = new System.Windows.Forms.Timer();
+            wftimer.Interval = intervalms;
+            wftimer.Tick += new EventHandler(timer_Tick);
+            executeRoutines();
+            wftimer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+            WriteToFile("Executing Routines...");
+            executeRoutines();
+            WriteToFile("Execution Complete!");
+
+        }
+        protected void Displaynotify()
+        {
+            try
+            {
+                notifyIcon1.Icon = new System.Drawing.Icon(Path.GetFullPath(@"image\graph.ico"));
+                notifyIcon1.Text = "IMGVisor Control";
+                notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipTitle = "Welcome IMG Visor Utility";
+                notifyIcon1.BalloonTipText = "Click Here to see details";
+                notifyIcon1.ShowBalloonTip(100);
+            }
+            catch (Exception ex)
+            {
+                var x = ex.Message;
+            }
+        }
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
+        private void executeRoutines()
+        {
+            try
+            {
+                for (int s = 1; s <= 10; s++)
+                {
+                    string configKey = "step_" + s;
+                    string stepValue = "";
+                    try
+                    {
+                        stepValue = ConfigurationManager.AppSettings.Get(configKey);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+
+                    if (!String.IsNullOrEmpty(stepValue))
+                    {
+                        executeStep(stepValue);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Capture Exception!" + ex.Message);
+            }
+        }
+
+        private void executeStep(string stepValue)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "CMD.exe",
+                    Arguments = "/c " + stepValue,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                WriteToFile("result:" + line);
+            }
+            WriteToFile("Capture call returned!");
+
+        }
+
+        public void WriteToFile(string Message)
+        {
+            Message = DateTime.Now + " " + Message;
+
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            Displaynotify();
+
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            notifyIcon1.Visible = false;
+        }
+    }
+}
